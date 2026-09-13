897cbd172e07f10c01f2c4460df85e3942020f96
chanura-madushan <chanuramadushan18@gmail.com>
2026-09-13 17:09:17 +0530

    cleaning data 2(clean data.py)

--- src/cleaneddata.py
+++ src/cleaneddata.py
@@ -5,3 +5,6 @@ df = pd.read_csv("data/netflix_titles.csv")
 print(df.shape)
 print(df["type"].value_counts())
 
+print(df[df["type"]=="Movie"][["title", "release_year", "duration"]].head())    #Show me the title, release year, and duration of movies.
+print(df[df["type"]=="TV Show"][["title", "release_year", "duration"]].head())  #Show me the title, release year, and duration of tv shows.
+
