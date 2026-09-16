cf9719d065a859d0f51e827a37980781d6e1046c
chanura-madushan <chanuramadushan18@gmail.com>
2026-09-16 11:27:48 +0530

    customised model behaviour

--- src/test_qwen.py
+++ src/test_qwen.py
@@ -1,11 +1,31 @@
 import ollama
-response = ollama.chat(
-     model="qwen2.5:3b",
-     messages=[
-         {
-             "role": "user",
-            "content": "Say hello to MovieGPT"
-         }
-     ]
-)
-print(response["message"]["content"])
+
+
+def ask_qwen(user_input):
+    response = ollama.chat(
+        model="qwen2.5:3b",
+         messages=[
+            {
+                "role": "system",
+                "content": """
+                        You are a movie chatbot.
+                        Classify the user's message into exactly one of these categories:
+                        GREETING
+                        RECOMMENDATION
+                        HELP
+                        GOODBYE
+                        Return only the category name.
+                        """
+            },
+            {
+                "role": "user",
+                "content": user_input
+            }
+        ]
+    )
+    return response["message"]["content"]
+
+
+user_input = input("You: ")
+answer = ask_qwen(user_input)
+print("Qwen:", answer)
